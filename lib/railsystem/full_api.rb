0250684810a3b6c4ac49770610c11c16749adfcd
Mohammad Satrio <wolfaeon@gmail.com>
2016-09-05 20:43:19 +0700

    Fix full API setup

--- lib/railsystem/full_api.rb
+++ lib/railsystem/full_api.rb
@@ -4,6 +4,7 @@ require "railsystem/railtie"
 module Railsystem
   class Railtie < ::Rails::Railtie
     initializer "railsystem.error_handling" do |app|
+      app.config.action_dispatch.show_exceptions = false
       app.middleware.swap("ActionDispatch::ShowExceptions",
                           "Railsystem::Middlewares::JsonError")
     end
