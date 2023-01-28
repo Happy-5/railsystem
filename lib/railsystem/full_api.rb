1c1b2de83449ee83aaa87913c4fdaa8412db44ca
Taufik Obet <obet@happy5.co>
2023-01-28 12:44:26 +0700

    Swap middleware on full_api

--- lib/railsystem/full_api.rb
+++ lib/railsystem/full_api.rb
@@ -5,8 +5,8 @@ module Railsystem
   class Railtie < ::Rails::Railtie
     initializer "railsystem.error_handling" do |app|
       app.config.action_dispatch.show_exceptions = false
-      app.middleware.swap("ActionDispatch::ShowExceptions",
-                          "Railsystem::Middlewares::JsonError")
+      app.middleware.swap(ActionDispatch::ShowExceptions,
+                          Railsystem::Middlewares::JsonError)
     end
   end
 end
